057ec46c601444b3d1340b209870c28e986268a4
rcwoshimao <mantoumiaoshen@gmail.com>
2023-04-19 16:10:37 -0700

    Created test for averageWithoutLowest method

--- ArrayTests.java
+++ ArrayTests.java
@@ -31,9 +31,9 @@ public class ArrayTests {
 
   @Test 
   public void testAvLowest(){
-    int[] input1 = {5,5,5,5}; 
-
-    assertArrayEquals(new int[]{4,3,2}, ArrayExamples.reversed(input1));
+    double[] input1 = {5.0,5.0,5.0,5.0}; 
+    double result = ArrayExamples.averageWithoutLowest(input1);
+    assertEquals(result, 5, 0.0001);
   }
 
 }
